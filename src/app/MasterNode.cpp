af15328d58d4bda29a7c40a7d9771ee5fd0688f9
Sebastian Maisch <sebastian.maisch@googlemail.com>
2017-05-27 15:08:09 +0200

    Updated library, adjusted to current tuio behavior.

--- src/app/MasterNode.cpp
+++ src/app/MasterNode.cpp
@@ -15,30 +15,9 @@ namespace viscom {
     MasterNode::MasterNode(ApplicationNodeInternal* appNode) :
         ApplicationNodeImplementation{ appNode }
     {
-
-
-#ifdef WITH_TUIO
-        auto tuioInputWrapper = std::make_unique<TuioInputWrapper>(GetConfig().tuioPort_, this);
-#endif
     }
 
     MasterNode::~MasterNode() = default;
-
-
-    void MasterNode::InitOpenGL()
-    {
-        ApplicationNodeImplementation::InitOpenGL();
-    }
-
-    void MasterNode::PreSync()
-    {
-        ApplicationNodeImplementation::PreSync();
-    }
-
-    void MasterNode::DrawFrame(FrameBuffer& fbo)
-    {
-        ApplicationNodeImplementation::DrawFrame(fbo);
-    }
 
     void MasterNode::Draw2D(FrameBuffer& fbo)
     {
@@ -56,56 +35,6 @@ namespace viscom {
         });
 
         ApplicationNodeImplementation::Draw2D(fbo);
-    }
-
-    void MasterNode::CleanUp()
-    {
-        ApplicationNodeImplementation::CleanUp();
-    }
-
-    bool MasterNode::KeyboardCallback(int key, int scancode, int action, int mods)
-    {
-#ifndef VISCOM_CLIENTGUI
-        ImGui_ImplGlfwGL3_KeyCallback(key, scancode, action, mods);
-        if (ImGui::GetIO().WantCaptureKeyboard) return true;
-#endif
-        return ApplicationNodeImplementation::KeyboardCallback(key, scancode, action, mods);
-    }
-
-    bool MasterNode::CharCallback(unsigned int character, int mods)
-    {
-#ifndef VISCOM_CLIENTGUI
-        ImGui_ImplGlfwGL3_CharCallback(character);
-        if (ImGui::GetIO().WantCaptureKeyboard) return true;
-#endif
-        return ApplicationNodeImplementation::CharCallback(character, mods);
-    }
-
-    bool MasterNode::MouseButtonCallback(int button, int action)
-    {
-#ifndef VISCOM_CLIENTGUI
-        ImGui_ImplGlfwGL3_MouseButtonCallback(button, action, 0);
-        if (ImGui::GetIO().WantCaptureMouse) return true;
-#endif
-        return ApplicationNodeImplementation::MouseButtonCallback(button, action);
-    }
-
-    bool MasterNode::MousePosCallback(double x, double y)
-    {
-#ifndef VISCOM_CLIENTGUI
-        ImGui_ImplGlfwGL3_MousePositionCallback(x, y);
-        if (ImGui::GetIO().WantCaptureMouse) return true;
-#endif
-        return ApplicationNodeImplementation::MousePosCallback(x, y);
-    }
-
-    bool MasterNode::MouseScrollCallback(double xoffset, double yoffset)
-    {
-#ifndef VISCOM_CLIENTGUI
-        ImGui_ImplGlfwGL3_ScrollCallback(xoffset, yoffset);
-        if (ImGui::GetIO().WantCaptureMouse) return true;
-#endif
-        return ApplicationNodeImplementation::MouseScrollCallback(xoffset, yoffset);
     }
 
 }
--- src/app/MasterNode.h
+++ src/app/MasterNode.h
@@ -21,17 +21,7 @@ namespace viscom {
         explicit MasterNode(ApplicationNodeInternal* appNode);
         virtual ~MasterNode() override;
 
-        void InitOpenGL() override;
-        void PreSync() override;
-        void DrawFrame(FrameBuffer& fbo) override;
         void Draw2D(FrameBuffer& fbo) override;
-        void CleanUp() override;
-
-        bool KeyboardCallback(int key, int scancode, int action, int mods) override;
-        bool CharCallback(unsigned int character, int mods) override;
-        bool MouseButtonCallback(int button, int action) override;
-        bool MousePosCallback(double x, double y) override;
-        bool MouseScrollCallback(double xoffset, double yoffset) override;
 
     };
 }
