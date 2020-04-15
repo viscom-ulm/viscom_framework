084b0996ba5a667c45ad0a15b14c23d9dd704298
Simon Besenthal <simon.besenthal@uni-ulm.de>
2020-04-15 11:26:27 +0200

    Loading imgui via conan

--- src/app/CoordinatorNode.cpp
+++ src/app/CoordinatorNode.cpp
@@ -93,9 +93,9 @@ namespace viscom {
     void CoordinatorNode::Draw2D(FrameBuffer& fbo)
     {
         fbo.DrawToFBO([this]() {
-            ImGui::ShowTestWindow();
-            ImGui::SetNextWindowPos(ImVec2(60, 60), ImGuiSetCond_FirstUseEver);
-            ImGui::SetNextWindowSize(ImVec2(550, 680), ImGuiSetCond_FirstUseEver);
+            ImGui::ShowDemoWindow();
+            ImGui::SetNextWindowPos(ImVec2(60, 60), ImGuiCond_FirstUseEver);
+            ImGui::SetNextWindowSize(ImVec2(550, 680), ImGuiCond_FirstUseEver);
             ImGui::StyleColorsClassic();
             if (ImGui::Begin("MasterTestWindow", nullptr))
             {
